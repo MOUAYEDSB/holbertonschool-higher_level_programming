79ca4779d177ffa24eb33af659d5bd1c1831d849
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-01 20:20:12 +0100

    get all states

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,28 +1,39 @@
 #!/usr/bin/python3
-"""Module that lists all states from the database"""
-
+"""
+a script that lists all states from the database hbtn_0e_0_usa
+"""
+import sys
 import MySQLdb
-from sys import argv
+
+
+def get_states(username, password, database):
+    conn = MySQLdb.connect(
+        host="localhost", port=3306, user=username, passwd=password, db=database
+    )
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+
+    results = cursor.fetchall()
+    for row in results:
+        print(row)
+
+    cursor.close()
+    conn.close()
 
 
 if __name__ == "__main__":
-    connection = MySQLdb.connect(host="localhost", port=3306,
-                                 user=argv[1], passwd=argv[2], db=argv[3])
-    """Connecting to the MySQL server"""
+    if len(sys.argv) != 4:
+        print(
+            "Usage: {} <mysql_username> <mysql_password> <database_name>".format(
+                sys.argv[0]
+            )
+        )
+        sys.exit(1)
 
-    cursor = connection.cursor()
-    """Creating a cursor object to execute queries"""
+    mysql_username = sys.argv[1]
+    mysql_password = sys.argv[2]
+    database_name = sys.argv[3]
 
-    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-    """Executing the SELECT query to retrieve all states"""
-
-    lines = cursor.fetchall()
-    """Fetching all the rows returned by the query"""
-
-    for line in lines:
-        """Display the results"""
-        print(line)
-
-    cursor.close()
-    connection.close()
-    """Closing the cursor and the connection"""
+    get_states(mysql_username, mysql_password, database_name)
