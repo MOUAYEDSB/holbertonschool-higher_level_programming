737f78812a8d20acfe254efd959a4d30a012caf9
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-02 14:16:53 +0100

    get a state

--- python-object_relational_mapping/10-model_state_my_get.py
+++ python-object_relational_mapping/10-model_state_my_get.py
@@ -1,38 +1,40 @@
 #!/usr/bin/python3
+"""
+Prints the State object with the name passed as an argument from the database hbtn_0e_6_usa
+"""
 
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 
-def print_state_by_name(mysql_username, mysql_password, database_name, state_name):
-    # Create the engine to connect to the MySQL server
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(mysql_username, mysql_password, database_name))
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        print(
+            "Usage: {} <mysql_username> <mysql_password> <database_name> <state_name>".format(
+                sys.argv[0]
+            )
+        )
+        sys.exit(1)
 
-    # Create a session to interact with the database
+    engine = create_engine(
+        "mysql+mysqldb://{}:{}@localhost/{}".format(
+            sys.argv[1], sys.argv[2], sys.argv[3]
+        ),
+        pool_pre_ping=True,
+    )
+
+    Base.metadata.bind = engine
+
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query the database to find the state by name
-    state = session.query(State).filter(State.name == state_name).first()
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
 
-    # Check if the state was found
-    if state is not None:
-        print(state.id)
-    else:
+    if state is None:
         print("Not found")
-
-if __name__ == "__main__":
-    # Check if all 4 arguments are provided
-    if len(sys.argv) != 5:
-        print("Usage: python script.py <mysql_username> <mysql_password> <database_name> <state_name>")
     else:
-        # Retrieve arguments from command line
-        mysql_username = sys.argv[1]
-        mysql_password = sys.argv[2]
-        database_name = sys.argv[3]
-        state_name = sys.argv[4]
+        print(state.id)
 
-        # Call the function to print the state
-        print_state_by_name(mysql_username, mysql_password, database_name, state_name)
+    session.close()
+	"""Close the session"""
