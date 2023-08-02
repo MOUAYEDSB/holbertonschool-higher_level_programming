742820ceb034cb98de4fa047c00ca496d58057b2
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-02 17:14:29 +0100

    update a state

--- python-object_relational_mapping/12-model_state_update_id_2.py
+++ python-object_relational_mapping/12-model_state_update_id_2.py
@@ -1,38 +1,25 @@
 #!/usr/bin/python3
-"""
-Changes the name of a State object from the database hbtn_0e_6_usa
-"""
+"""Module that changes the name of a State object from the database"""
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print(
-            "Usage: {} <mysql_username> <mysql_password> <database_name>".format(
-                sys.argv[0]
-            )
-        )
-        sys.exit(1)
 
-    engine = create_engine(
-        "mysql+mysqldb://{}:{}@localhost/{}".format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True,
-    )
-
-    Base.metadata.bind = engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
+    """Creating a connection to the MySQL server"""
 
     Session = sessionmaker(bind=engine)
+    """Creating a session to interact with the database"""
     session = Session()
 
-    state_to_update = session.query(State).filter_by(id=2).first()
+    session.query(State).filter(State.id == 2).update({"name": "New Mexico"})
 
-    if state_to_update:
-        state_to_update.name = "New Mexico"
-        session.commit()
+    session.commit()
 
     session.close()
+    """Close the session"""
