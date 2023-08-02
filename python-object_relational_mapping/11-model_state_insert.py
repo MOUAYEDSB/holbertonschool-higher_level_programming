0e6149ba73e434429361ec14e266a7eae1916b7d
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-02 17:12:43 +0100

    add new state

--- python-object_relational_mapping/11-model_state_insert.py
+++ python-object_relational_mapping/11-model_state_insert.py
@@ -1,32 +1,26 @@
 #!/usr/bin/python3
-"""
-Adds the State object "Louisiana" to the database hbtn_0e_6_usa
-"""
+"""Module that adds the State object “Louisiana” to the database"""
 
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
-        sys.exit(1)
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
-
-    Base.metadata.bind = engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.
+                           format(argv[1], argv[2], argv[3],
+                                  pool_pre_ping=True))
+    """Creating a connection to the MySQL server"""
 
     Session = sessionmaker(bind=engine)
+    """Creating a session to interact with the database"""
     session = Session()
 
     new_state = State(name="Louisiana")
-
     session.add(new_state)
     session.commit()
-
-    print(new_state.id)
+    print("{}".format(new_state.id))
 
     session.close()
+    """Close the session"""
