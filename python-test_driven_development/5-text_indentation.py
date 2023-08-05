bb52fe0af7b34b44883f9125f93a8193fa2c798b
MOUAYEDSB <sabbaghmouayed@gmail.com>
2023-08-05 01:28:16 +0100

    text identitation

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
+'''
+This module provides a function `text_indentation` for manipulating
+text strings
+'''
 
 
 def text_indentation(text):
-    """
-        Prints a new line
-
-        Raise:
-            TypeError: text must be a string
-    """
-    if not isinstance(text, str):
+    '''
+    This function prints a text with two new lines after each of these
+    characters: ., ? and :
+    Args:
+        text (str): The text to process. Must be a string
+    Raises:
+        TypeError: If `text` is not a string.
+    '''
+    if type(text) != str:
         raise TypeError("text must be a string")
 
-    new_text = ""
-    for string in text:
-        new_text += string
-        if string in [".", "?", ":"]:
-            new_text += "\n\n"
-
-    print(new_text)
-
-
-if __name__ == "__main__":
-    import doctest
-    doctest.testfile("tests/5-text_indentation.txt")
+    characters = ['.', '?', ':']
+    result = ""
+    for char in text:
+        result += char
+        if char in (characters):
+            result += "\n\n"
+    split_line = result.split("\n")
+    for i in range(len(split_line)):
+        if i == len(split_line) - 1:
+            print(split_line[i].strip(), end="")
+        else:
+            print(split_line[i].strip())
